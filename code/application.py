7b9170516a31db253248d62c46ffe9a382eb7b55
pronoy1004 <pronoy1004@gmail.com>
2021-12-07 00:30:05 -0500

    Update application.py

--- code/application.py
+++ code/application.py
@@ -1,83 +1,112 @@
 import database
+def run_app():
+    inp = 'y'
+
+
+    
+    print("Welcome to our Database Project, select the options to choose what you'd like to see :   ")
+    
+    print("Enter")
+    
+    print("1: ")
+    
+    print("2: ")
+    
+    print("3: ")
+    
+    print("4: ")
+    
+    print("5: ")
+    
+    print("Enter x to Exit")
+    
+    inp = input()
+    if(inp == 'x'):
+        return
+    elif(inp == '1'):
+        query.query1()
+
+    elif(inp == '2'):
+        query.query2()
+
+    elif(inp == '3'):
+        query.query3()
+
+    elif(inp == '4'):
+        query.query4()
+
+    elif(inp == '5'):
+        query.query5()
+
+    else:
+        print("Incorrect choice, please select again! ")
+
+    print("Thank you!")
+
+
+def set_up():
+    log = 1 
+    while(log == 1):
+        
+        print("Hello User! Login/Register? : ")
+
+        
+        print("1.Login: ")
+            
+        print("2.Register: ")
+
+        log=input()
+
+        
+        if(log == '1'):
+            y = query.login()
+            if(y == 1):
+                run_app()
+            elif(y == 0):
+                print("Incorrect User Name")
+    return log
 
 if __name__ == "__main__":
     query = database.ApplicationQueries()
 
     #### Login/Register ####
 
-    print("Hello User! Login/Register? : ")
+    log = set_up()
 
-    log = 'y'
+    if(log == '2'):
+        y = query.register()
+        if(y == 1):
+            set_up()
+        elif(y == 0):
+            print("Error!")
+    else:
+        print("Incorrect choice, please select again! ")
+
+    # print("Hello User! Login/Register? : ")
+
     
-    print("1.Login: ")
+    # print("1.Login: ")
         
-    print("2.Register: ")
+    # print("2.Register: ")
 
-    log=input()
+    # log=input()
 
-    def logreg():
-        while(log!='x'):
+    
+    # if(log == '1'):
+    #     y = query.login()
+    #     if(y == 1):
+    #         run_app()
+    #     elif(y == 0):
+    #         print("Incorrect User Name")
 
-            if(log == 'n'):
-                break
-            elif(log == 1):
-                y = query.login()
-                if(y == 1):
-                    run_app()
-                elif(y == 0):
-                    print("Incorrect User Name")
+    # elif(log == '2'):
+    #     query.register()
+    #     if(y == 1):
+    #         ()
+    #     elif(y == 0):
+    #         print("Error!")
 
-            elif(log == 2):
-                query.register()
-                if(y == 1):
-                    logreg()
-                elif(y == 0):
-                    print("Error!")
- 
-            else:
-                print("Incorrect choice, please select again! ")
+    # else:
+    #     print("Incorrect choice, please select again! ")
 
 
-    def run_app():
-        inp = 'y'
-
-        while(inp != 'x'):
-            
-            print("Welcome to our Database Project, select the options to choose what you'd like to see :   ")
-            
-            print("Enter")
-            
-            print("1: ")
-            
-            print("2: ")
-            
-            print("3: ")
-            
-            print("4: ")
-            
-            print("5: ")
-            
-            print("Enter x to Exit")
-            
-            inp = input()
-            if(inp == 'n'):
-                break
-            elif(inp == 1):
-                query.query1()
-
-            elif(inp == 2):
-                query.query2()
-
-            elif(inp == 3):
-                query.query3()
-
-            elif(inp == 4):
-                query.query4()
-
-            elif(inp == 5):
-                query.query5()
-
-            else:
-                print("Incorrect choice, please select again! ")
-
-        print("Thank you!")
